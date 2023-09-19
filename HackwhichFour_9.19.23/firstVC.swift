2ab46c2c170ce73646c1e646f75743bb85296781
cazlito <142947671+cazlito@users.noreply.github.com>
2023-09-19 13:37:01 -1000

    added labels to vc and created individual swift files

---
+++ HackwhichFour_9.19.23/firstVC.swift
@@ -0,0 +1,8 @@
+//
+//  firstVC.swift
+//  HackwhichFour_9.19.23
+//
+//  Created by Cazandra Rufo on 9/19/23.
+//
+
+import Foundation
---
+++ HackwhichFour_9.19.23/secondVC.swift
@@ -0,0 +1,8 @@
+//
+//  secondVC.swift
+//  HackwhichFour_9.19.23
+//
+//  Created by Cazandra Rufo on 9/19/23.
+//
+
+import Foundation
